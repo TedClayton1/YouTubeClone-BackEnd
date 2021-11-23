5824c2159fbe9db32af1a54cb1748adad7fb48bd
Dustin Barker <barkerd85@yahoo.com>
2021-11-23 12:42:00 -0700

    Updated Admin.py + Installed Apps

--- youtube_clone_project/myproject/settings.py
+++ youtube_clone_project/myproject/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'youtube_clone.apps.YoutubeCloneConfig',
-    'myapp.apps.MyappConfig'
+    'youtube_clone',
+    'myapp'
 ]
 
 MIDDLEWARE = [
