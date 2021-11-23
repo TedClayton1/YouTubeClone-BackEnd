5ec8319f2e17e39b2b27f003081a1da5eca4e68b
Dustin Barker <barkerd85@yahoo.com>
2021-11-23 12:30:30 -0700

    Added Reply Model

--- youtube_clone_project/myapp/models.py
+++ youtube_clone_project/myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Reply(models.Model):
-    video_id = models.CharField(max_length=50)
-    comment = models.CharField(max_length=50)
-    likes = models.IntegerField(default=0)
-    dislikes = models.IntegerField(default=0)
+    comment = models.ForeignKey('youtube_clone.Comment', on_delete=models.CASCADE)
+    reply = models.CharField(max_length=100)
+    
+    
