f93bd177ace98568c0b3ddfa1683beab99a6434c
Dustin Barker <barkerd85@yahoo.com>
2021-11-23 13:07:52 -0700

    Updated apps

--- youtube_clone_project/myapp/views.py
+++ youtube_clone_project/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .serializers import ReplySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from models import Reply
 
 # Create your views here.
--- youtube_clone_project/youtube_clone/views.py
+++ youtube_clone_project/youtube_clone/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from models import Comment
+from .serializers import CommentSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
