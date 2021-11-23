7a0da3f12e7cb3aa572b8d2f99499d2af783b01f
Dustin Barker <barkerd85@yahoo.com>
2021-11-22 17:47:45 -0700

    Added the models file and synced project with mySQL

---
+++ youtube_clone_project/myproject/local_settings.py
@@ -0,0 +1,23 @@
+SECRET_KEY = 'a!jn2bomp2enq7!%u!law)l0s%pk0g8mk6*580uw7+%*ka5@7b'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'youtube_clone',
+        'USER': 'root',
+        'PASSWORD': '2Bearcats',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+
+    }
+}
---
+++ youtube_clone_project/youtube_clone/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db.models.fields import IntegerField
+
+# Create your models here.
+class Comment(models.Model):
+    video_id = models.IntegerField(default=0)
+    comment = models.CharField(max_length=50)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
